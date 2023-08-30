bba896494357063715e05db381d4bd9b4e556e2c
thao hoang <huongthaoh@robins-MacBook-Air.local>
2023-08-30 16:32:29 -0500

    upd pages content

--- client/src/pages/Services/Courses.js
+++ client/src/pages/Services/Courses.js
@@ -9,15 +9,15 @@ function Courses() {
         <div className="services-coursesContainer">
         <div className="services-coursesOne">
             <div className="services-coursesLeft">
-                <h5>24/07 - 03/11/2023</h5>
-                <h3>Khoá luyện thi tháng 10</h3>
+                <h5>23/11 - 08/03/2024</h5>
+                <h3>Khoá luyện thi tháng 3</h3>
                 
             </div>
             <div className="services-coursesRight">
                 <div className="services-coursesBoxOne">
                     <h2>01</h2>
                     <h3>Lớp căn bản</h3>
-                    <p>Lịch học: tối thứ 3 và thứ 6 từ 18h-20h.</p>
+                    <p>Lịch học: tối thứ 5 và thứ 6 từ 20h20-22h20.</p>
                     <p>Đầu vào: 1050+ SAT, 6.5+ IELTS, hoặc C1+ </p>
 
                 </div>
@@ -25,7 +25,7 @@ function Courses() {
                 <div className="services-coursesBoxTwo">
                     <h2>02</h2>
                     <h3>Lớp nâng cao</h3>
-                    <p>Lịch học: tối thứ 2 và thứ 3 từ 20h15-22h15.</p>
+                    <p>Lịch học: tối thứ 5 và thứ 6 từ 18h-20h.</p>
                     <p>Đầu vào: 1250+ SAT</p>
                 </div>
             </div>
@@ -33,15 +33,15 @@ function Courses() {
 
         <div className="services-coursesTwo">
             <div className="services-coursesLeft">
-                <h5>27/08 - 29/11/2023</h5>               
-                <h3>Khoá luyện thi tháng 11</h3>
+                <h5>13/01 - 28/04/2024</h5>               
+                <h3>Khoá luyện thi tháng 5</h3>
             </div>
 
             <div className="services-coursesRight">
                 <div className="services-coursesBoxOne">
                     <h2>01</h2>
                     <h3>Lớp căn bản</h3>
-                    <p>Lịch học: tối thứ 4 và CN từ 20h15-22h25.</p>
+                    <p>Lịch học: tối thứ 7 và CN từ 20h20-22h20.</p>
                     <p>Đầu vào: 1050+ SAT, 6.5+ IELTS, hoặc C1+ </p>
 
                 </div>
@@ -49,11 +49,36 @@ function Courses() {
                 <div className="services-coursesBoxTwo">
                     <h2>02</h2>
                     <h3>Lớp nâng cao</h3>
-                    <p>Lịch học: tối thứ 2 và CN từ 17h50-20h.</p>
-                    <p>Đầu vào: 1050+ SAT, 6.5+ IELTS, hoặc C1+</p>
+                    <p>Lịch học: tối thứ 7 và CN từ 18h-20h.</p>
+                    <p>Đầu vào: 1250+ SAT</p>
                 </div>
             </div>
         </div>   
+
+        <div className="services-coursesTwo">
+            <div className="services-coursesLeft">
+                <h5>19/02 - 28/05/2024</h5>               
+                <h3>Khoá luyện thi tháng 6</h3>
+            </div>
+
+            <div className="services-coursesRight">
+                <div className="services-coursesBoxOne">
+                    <h2>01</h2>
+                    <h3>Lớp căn bản</h3>
+                    <p>Lịch học: tối thứ 3 và 4 từ 18h-20h.</p>
+                    <p>Đầu vào: 1050+ SAT, 6.5+ IELTS, hoặc C1+ </p>
+
+                </div>
+
+                <div className="services-coursesBoxTwo">
+                    <h2>02</h2>
+                    <h3>Lớp nâng cao</h3>
+                    <p>Lịch học: tối thứ 3 và 4 từ 20h20-22h20.</p>
+                    <p>Đầu vào: 1250+ SAT</p>
+                </div>
+            </div>
+        </div>  
+
 
         </div>     
 
